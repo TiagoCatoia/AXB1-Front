4ed180d7a2b38be4d2180c4b207a14f87ca09795
Lucas Jundi Hikazudani <lhjundi@proton.me>
2024-10-07 17:07:04 -0300

    Add tailwinf config options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  "./src/index.css",
+  "./src/App.jsx",
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
