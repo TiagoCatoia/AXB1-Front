0489bb288b3aa89911e1a740bff3a4ee5ba56651
Lucas Jundi Hikazudani <lhjundi@proton.me>
2024-10-07 16:46:22 -0300

    Add Logo component

---
+++ src/LoginPage/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <img
+      src="https://cdn.builder.io/api/v1/image/assets/TEMP/1cb6bde722cf29081ecd8343e5fb57787e2c94ebbe2d506a5ca31c6bb0c0ba9c?placeholderIfAbsent=true&apiKey=134860f90afa4b4bbe840c2326e96abf"
+      alt="Machado Genomics Logo"
+      className="object-contain max-w-full aspect-[1.77] w-[230px]"
+    />
+  );
+}
+
+export default Logo;
