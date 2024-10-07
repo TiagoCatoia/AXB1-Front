e5db2baccef10b90fd933b9f1ecbdac05f4f9a61
Lucas Jundi Hikazudani <lhjundi@proton.me>
2024-10-07 16:44:58 -0300

    Add LoginPage component

---
+++ src/LoginPage/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import WelcomeImage from './WelcomeImage';
+
+function LoginPage() {
+  return (
+    <main className="flex overflow-hidden flex-col justify-center p-4 bg-gray-600">
+      <div className="py-7 pr-9 pl-20 bg-white rounded-[50px] max-md:px-5 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col">
+          <LoginForm />
+          <WelcomeImage />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default LoginPage;
